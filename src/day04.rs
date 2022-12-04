393ae61e59032f43426e6f8aebc8479dde3533f1
Fran Casas <nflamel@gmail.com>
2022-12-04 22:43:33 +0100

    day 4, part 2

--- src/day04.rs
+++ src/day04.rs
@@ -7,6 +7,10 @@ impl Section {
     fn contains(&self, s: &Section) -> bool {
         self.0 <= s.0 && self.1 >= s.1
     }
+
+    fn overlaps(&self, s: &Section) -> bool {
+        self.0 <= s.0 && s.0 <= self.1 || s.1 <= self.1 && self.0 <= s.1
+    }
 }
 
 struct Pair(Section, Section);
@@ -31,15 +35,28 @@ impl Pair {
         Pair(left, right)
     }
 
-    fn overlaps(&self) -> bool {
+    fn contains(&self) -> bool {
         let Pair(left, right) = self;
 
         left.contains(&right) || right.contains(&left)
+    }
+
+    fn overlaps(&self) -> bool {
+        self.0.overlaps(&self.1) || self.1.overlaps(&self.0)
     }
 }
 
 #[aoc(day4, part1)]
 pub fn solve_part1(input: &str) -> usize {
+    input
+        .split("\n")
+        .map(|line| Pair::from_line(line))
+        .filter(|pair| pair.contains())
+        .count()
+}
+
+#[aoc(day4, part2)]
+pub fn solve_part2(input: &str) -> usize {
     input
         .split("\n")
         .map(|line| Pair::from_line(line))
@@ -64,6 +81,18 @@ mod tests {
     }
 
     #[test]
+    fn test_day4_part2() {
+        let input = "2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8";
+
+        assert_eq!(4, solve_part2(input));
+    }
+
+    #[test]
     fn test_section_contains() {
         let a = Section(1, 1);
         let b = Section(1, 1);
@@ -77,10 +106,34 @@ mod tests {
     }
 
     #[test]
-    fn test_pair_overlaps() {
+    fn test_section_overlaps() {
+        let a = Section(1, 2);
+        let b = Section(2, 3);
+        let c = Section(1, 2);
+        let d = Section(3, 8);
+
+        assert!(a.overlaps(&b));
+        assert!(b.overlaps(&a));
+        assert!(!c.overlaps(&d));
+        assert!(!d.overlaps(&c));
+    }
+
+    #[test]
+    fn test_pair_contains() {
         let a = Pair(Section(1, 1), Section(1, 1));
         let b = Pair(Section(1, 2), Section(1, 1));
         let c = Pair(Section(1, 2), Section(2, 3));
+
+        assert!(a.contains());
+        assert!(b.contains());
+        assert!(!c.contains());
+    }
+
+    #[test]
+    fn test_pair_overlaps() {
+        let a = Pair(Section(1, 1), Section(1, 1));
+        let b = Pair(Section(1, 2), Section(1, 1));
+        let c = Pair(Section(1, 2), Section(3, 4));
 
         assert!(a.overlaps());
         assert!(b.overlaps());
