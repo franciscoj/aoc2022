a897a5ae79187a571db21ce14d556484a23d691d
Fran Casas <nflamel@gmail.com>
2022-12-08 17:56:26 +0100

    day 6, part 1

---
+++ src/day06.rs
@@ -0,0 +1,45 @@
+use aoc_runner_derive::aoc;
+
+#[aoc(day6, part1)]
+pub fn solve_part1(input: &str) -> usize {
+    let msg: Vec<char> = input.chars().collect();
+    let mut is_marker = false;
+
+    input.chars().fold(1 as usize, |acc, _x| {
+        if acc < 4 {
+            acc + 1
+        } else {
+            if is_marker {
+                acc
+            } else {
+                let chars = &msg[acc - 4..acc];
+                is_marker = chars[0] != chars[1]
+                    && chars[1] != chars[2]
+                    && chars[2] != chars[3]
+                    && chars[0] != chars[2]
+                    && chars[0] != chars[3]
+                    && chars[1] != chars[3];
+
+                if is_marker {
+                    acc
+                } else {
+                    acc + 1
+                }
+            }
+        }
+    })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_day6_part1() {
+        assert_eq!(7, solve_part1("mjqjpqmgbljsphdztnvjfqwrcgsmlb"));
+        assert_eq!(5, solve_part1("bvwbjplbgvbhsrlpgdmjqwftvncz"));
+        assert_eq!(6, solve_part1("nppdvjthqldpwncqszvftbrmjlhg"));
+        assert_eq!(10, solve_part1("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"));
+        assert_eq!(11, solve_part1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"));
+    }
+}
